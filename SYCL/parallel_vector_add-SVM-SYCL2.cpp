bd72299e81f01437e43bbcf82eb2a8f1e85df818
Ronan Keryell <ronan.keryell@xilinx.com>
2015-09-29 14:44:11 +0100

    Future fine grain system SVM version of SYCL vector add example

---
+++ SYCL/parallel_vector_add-SVM-SYCL2.cpp
@@ -0,0 +1,26 @@
+/* RUN: %{execute}%s | %{filecheck} %s
+   CHECK: Result:
+   CHECK-NEXT: 6 8 11
+*/
+#include <CL/sycl.hpp>
+#include <iostream>
+
+constexpr size_t N = 3;
+using Vector = float[N];
+
+int main() {
+  Vector a = { 1, 2, 3 };
+  Vector b = { 5, 6, 8 };
+  Vector c;
+
+  // Enqueue a synchronous parallel kernel
+  cl::sycl::parallel_for<class vector_add>(cl::sycl::range<1> { N },
+                                           [=] (cl::sycl::id<1> index) {
+                                             c[index] = a[index] + b[index];
+                                           });
+
+  std::cout << std::endl << "Result:" << std::endl;
+  for(auto e : c)
+    std::cout << e << " ";
+  std::cout << std::endl;
+}
