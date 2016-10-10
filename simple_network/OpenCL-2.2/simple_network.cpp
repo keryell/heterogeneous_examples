d37697f9a6b91ecfe3e1410625f1a61fb06b688a
Ronan Keryell <ronan.keryell@xilinx.com>
2016-10-10 20:10:08 +0100

    Add a host part to simple_network example

---
+++ simple_network/OpenCL-2.2/simple_network.cpp
@@ -0,0 +1,57 @@
+/** Host part of the simple networking application
+
+    This starts the L2 forwarding application and loop on updating the
+    forwarding table according to some external user-interface.
+ */
+
+#include <boost/compute.hpp>
+#include <xilinx/networking>
+
+int main() {
+  // Create the OpenCL context to attach resources on the device
+  auto context = boost::compute::system::default_context();
+  // Create the OpenCL command queue to control the device
+  auto command_queue = boost::compute::system::default_queue();
+
+  // Create a device default queue
+  // [...]
+  
+  /* Construct an OpenCL program from the source string
+
+     Boost.Compute caches the binary, so on FPGA you pay the lengthy
+     compilation only... once. */
+  auto program = boost::compute::program::create_with_source_file
+    ("simple_network.cl", boost::compute::system::default_context());
+
+  program.build();
+
+  /* Start the dummy kernel force_init just to initialize the whole
+     program on the accelerator, which start the whole application by
+     side effect
+  */
+  auto force_init = boost::compute::kernel { program, "force_init" };
+
+  /* Launch force_init kernel with 1 work-item, forcing the
+     initialization of the program-scope objects */
+  command_queue.enqueue_task(force_init);
+
+  auto update = boost::compute::kernel { program, "update_forward_table" };
+
+  // The forwarding table
+  xlnx::util::set<xlnx::network::ethernet::address, 1000> forward;
+  // And the buffer to send it to the device
+  boost::compute::buffer fb { context, sizeof(forward), CL_MEM_READ_ONLY };
+  // The update_forward_table kernel will take this buffer
+  update.set_args(fb);
+ 
+  for (; /* ever */ ;) {
+    // Get some forwarding update from some external user interface...
+    ux.update(forward);
+    // Send the forwarding table top the accelerator
+    command_queue.enqueue_write_buffer(fb, 0 /* Offset */,
+				       sizeof(forward), &forward);
+    /* Launch the update_forward_table kernel with 1 work-item to lock
+       and update the table on the device */
+    command_queue.enqueue_task(update);
+  }
+}
